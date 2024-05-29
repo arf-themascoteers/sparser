aac4a24c8cd31ac2f0b2d5f1878e8cb562626fa0
Arif <arif@themascoteers.com>
2024-05-29 15:42:19 +1000

    start

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from task_runner import TaskRunner
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tasks = {
+        "algorithms" : ["zhang"],
+        "datasets" : ["indian_pines"],
+        "target_sizes" : [25, 30, 5, 10, 15, 20]
+    }
+    ev = TaskRunner(tasks,1,10,"2.csv",skip_all_bands=True)
+    ev.evaluate()
